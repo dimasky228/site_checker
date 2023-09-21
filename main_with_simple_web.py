7e0ce91c4cbf99e7535026439183f51ba92745c8
by_d1masky <by_d1masky@mail.ru>
2023-09-21 17:08:38 +0200

    version 1.0 betatest

---
+++ main_with_simple_web.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+import httpx
+import requests
+
+expressions_dict = {
+    200: "Access is allowed - OK.",
+    301: "Permanent Redirect - OK",
+    302: "Temporary Redirect - OK",
+    400: "Isn't working",
+    401: "Isn't working",
+    403: "Isn't working",
+    404: "Not Found, Isn't working",
+    500: "Isn't working"
+}
+
+app = FastAPI()
+
+
+@app.post("/check_website")
+async def check_website(url: str):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+        return {"status_code": expressions_dict[response.status_code]}
+
+
+@app.get('/')
+def home():
+    return ("hello")
